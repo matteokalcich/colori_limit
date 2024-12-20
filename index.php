3c54595bde28d32b44a06fb3b0230eb07321d8ad
Matteo Kalcich <matteokalcich@gmail.com>
2024-12-20 21:38:23 +0100

    Final version

--- index.php
+++ index.php
@@ -1,18 +1,49 @@
 <?php
 session_start();
 require_once ('conn.php');
-if(!isset($_SESSION['nColoriPerPagina'])){
-    $_SESSION['nColoriPerPagina'] = null;
+
+// Inizializzazione sessioni per la paginazione
+if (!isset($_SESSION['nColoriPerPagina'])) {
+    $_SESSION['nColoriPerPagina'] = 10; // Default 10 record per pagina
 }
 
-if(!isset($_SESSION['opzioneCorrente'])){
-    $_SESSION['opzioneCorrente'] = null;
-}
-
-if(!isset($_SESSION['paginaCorrente'])){
+if (!isset($_SESSION['paginaCorrente'])) {
     $_SESSION['paginaCorrente'] = 1;
 }
 
+if (isset($_POST['opz'])) {
+    $_SESSION['nColoriPerPagina'] = ($_POST['opz'] === 'all') ? 'all' : intval($_POST['opz']);
+    $_SESSION['paginaCorrente'] = 1; // Reset alla prima pagina quando cambia la quantità per pagina
+}
+
+// Gestione navigazione pagine
+if (isset($_POST['rightArrow'])) {
+    $_SESSION['paginaCorrente']++;
+}
+
+if (isset($_POST['leftArrow']) && $_SESSION['paginaCorrente'] > 1) {
+    $_SESSION['paginaCorrente']--;
+}
+
+// Determinazione LIMIT e OFFSET
+$nColoriPerPagina = $_SESSION['nColoriPerPagina'];
+$paginaCorrente = $_SESSION['paginaCorrente'];
+
+$limit = ($nColoriPerPagina === 'all') ? '' : "LIMIT $nColoriPerPagina OFFSET " . (($paginaCorrente - 1) * $nColoriPerPagina);
+
+// Query per recuperare i dati
+$query = "SELECT * FROM Colori $limit;";
+$result = mysqli_query($db, $query);
+
+// Controllo per i pulsanti di navigazione
+$prevQuery = "SELECT COUNT(*) as total FROM Colori WHERE id < (SELECT MIN(id) FROM Colori $limit);";
+$nextQuery = "SELECT COUNT(*) as total FROM Colori WHERE id > (SELECT MAX(id) FROM Colori $limit);";
+
+$prevResult = mysqli_query($db, $prevQuery);
+$nextResult = mysqli_query($db, $nextQuery);
+
+$hasPrev = $prevResult && mysqli_fetch_assoc($prevResult)['total'] > 0;
+$hasNext = $nextResult && mysqli_fetch_assoc($nextResult)['total'] > 0;
 ?>
 
 <!DOCTYPE html>
@@ -23,113 +54,29 @@ if(!isset($_SESSION['paginaCorrente'])){
     <title>Colori</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-
     <form action="" method="post">
-
         <p>Scegli quanti colori vedere:&nbsp;</p>
-
         <select name="opz">
-
-            <option value="10">10</option>
-            <option value="25">25</option>
-            <option value="50">50</option>
-            <option value="all">Tutti</option>
-            
+            <option value="10" <?php echo $nColoriPerPagina === 10 ? 'selected' : ''; ?>>10</option>
+            <option value="25" <?php echo $nColoriPerPagina === 25 ? 'selected' : ''; ?>>25</option>
+            <option value="50" <?php echo $nColoriPerPagina === 50 ? 'selected' : ''; ?>>50</option>
+            <option value="all" <?php echo $nColoriPerPagina === 'all' ? 'selected' : ''; ?>>Tutti</option>
         </select>
-
         <button type="submit" name="Invia">Mostra</button>
-
-        <button type="submit" name="leftArrow" <?php echo $_SESSION['paginaCorrente'] == 1 ? 'disabled' : ''; ?>><</button>
-
-        <button type="submit" name="rightArrow">></button>
-
+        <button type="submit" name="leftArrow" >&lt;</button>
+        <button type="submit" name="rightArrow">&gt;</button>
     </form>
 
-    <?php
-
-    if(isset($_POST['Invia'])){
-
-        if($_POST['opz'] != 'all'){
-
-            $query = 'SELECT * FROM Colori LIMIT '.$_POST['opz'].';';
-            $r = mysqli_query($db, $query);
-
-            while($a=mysqli_fetch_array($r)){
-
-                echo '<div class="divIntero"><h3>'.$a['id'].'</h3><div class="coloreCella" style="background-color:'.$a['colore'].'"></div><h4>Colore: '.$a['colore'].'</h4></div>';
-                
-                $_SESSION['nColoriPerPagina'] = $a['id'];
-            }
-
-            $_SESSION['opzioneCorrente'] = $_POST['opz'];
-
-        } else {
-
-            $query = 'SELECT * FROM Colori;';
-            $r = mysqli_query($db, $query);
-
-            while($a=mysqli_fetch_array($r)){
-
-                echo '<div class="divIntero"><h3>'.$a['id'].'</h3><div class="coloreCella" style="background-color:'.$a['colore'].'"></div><h4>Colore: '.$a['colore'].'</h4></div>';
-            }
-
-        }
-    }
-
-    if(isset($_POST['rightArrow'])){
-
-        $offset = $_SESSION['paginaCorrente'] * $_SESSION['opzioneCorrente'];
-        $query = 'SELECT * FROM Colori WHERE id > '.$_SESSION['nColoriPerPagina'].' LIMIT '.$_SESSION['opzioneCorrente'].';';
-        $r = mysqli_query($db, $query);
-
-        $resultCount = 0;
-        while($a=mysqli_fetch_array($r)){
-
-            echo '<div class="divIntero"><h3>'.$a['id'].'</h3><div class="coloreCella" style="background-color:'.$a['colore'].'"></div><h4>Colore: '.$a['colore'].'</h4></div>';
-            
-            $resultCount++;
-        }
-
-        if ($resultCount > 0) {
-            $_SESSION['paginaCorrente']++;
-        }
-    }
-
-
-    if(isset($_POST['leftArrow'])){
-
-        $offset = $_SESSION['paginaCorrente'] * $_SESSION['opzioneCorrente'];
-        $query = 'SELECT * FROM Colori WHERE id > '.$_SESSION['nColoriPerPagina'].' LIMIT '.$_SESSION['opzioneCorrente'].';';
-        $r = mysqli_query($db, $query);
-
-        $resultCount = 0;
-        while($a=mysqli_fetch_array($r)){
-
-            echo '<div class="divIntero"><h3>'.$a['id'].'</h3><div class="coloreCella" style="background-color:'.$a['colore'].'"></div><h4>Colore: '.$a['colore'].'</h4></div>';
-            
-            $resultCount--;
-        }
-
-        if ($resultCount > 0) {
-            $_SESSION['paginaCorrente']--;
-        }
-    }
-
-    // if(isset($_POST['leftArrow']) && $_SESSION['paginaCorrente'] > 1){
-
-    //     $_SESSION['paginaCorrente']--;
-    //     $offset = ($_SESSION['paginaCorrente'] - 1) * $_SESSION['opzioneCorrente'];
-    //     $query = 'SELECT * FROM Colori LIMIT '.$_SESSION['opzioneCorrente'].' OFFSET '.$offset.';';
-    //     $r = mysqli_query($db, $query);
-
-    //     while($a=mysqli_fetch_array($r)){
-
-    //         echo '<div class="divIntero"><h3>'.$a['id'].'</h3><div class="coloreCella" style="background-color:'.$a['colore'].'"></div><h4>Colore: '.$a['colore'].'</h4></div>';
-    //     }
-    // }
-
-    ?>
-
+    <div class="colori-container">
+        <?php while ($row = mysqli_fetch_assoc($result)): ?>
+            <div class="divIntero">
+                <h3><?php echo $row['id']; ?></h3>
+                <div class="coloreCella" style="background-color: <?php echo $row['colore']; ?>;"></div>
+                <h4>Colore: #<?php echo $row['colore']; ?></h4>
+            </div>
+        <?php endwhile; ?>
+    </div>
 </body>
 </html>
